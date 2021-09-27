2f7b1eae36a51345aec07253e9347181f4d18528
woozoo_zinc <your_zinc@ewhain.net>
2021-09-27 23:43:08 +0900

    (필수) 20055번 진행중

--- 9월 21일 - 브루트 포스/2108.cpp
+++ 9월 21일 - 브루트 포스/2108.cpp
@@ -1,25 +1,58 @@
+//진행중입니다
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <cmath> // floor()
+#include <map>
 using namespace std;
 
 int main()
 {
-    long int r, b; // r=빨간색 타일의 수, b=갈색 타일의 수
-    // 8<=r<<=5,000  1<=b<=2,000,000
-    // r+b = 2,005,000
+    //Input
+    int n;
+    cin>>n;
     
-    cin>>r>>b;
+    vector<int> m(n);
+    map<int,int> mm;
     
-    int c = r+b; // c= 직사각형 넓이
+    int s,small,big; //s= 합계, small=최솟값, big=최댓값
     
-    for(int i=3; i<c; i++)
+    cin>>m[0];
+    s=small=big=m[0];
+    
+    for(int i=1; i<n; i++){
+        cin>>m[i];
+        s+=m[i];
+    }
+        
+    //Ouput
+    //산술평균 - 반올림
+    cout<<floor((float)s/n+0.5)<<"\n";
+
+    sort(m.begin(),m.end());
+    
+    //중앙값
+    cout<<m[n/2]<<"\n";
+
+    //최빈값
+    map<int, int> mode; //최빈값 map
+    
+    int j= m[0];
+    int cnt= 1;
+    for(int i=1; i<n; i++)
     {
-        if(c%i==0 && (c/i-2)*(i-2)==b)
+        if(j==m[i]) cnt++;
+        else
         {
-            cout<<c/i<<" "<<i;
-            break;
+            mode.insert(make_pair(cnt, j));
+            j=m[i];
+            cnt=1;
         }
     }
+    
+
+    //범위
+    cout<<*(m.end()-1)-*(m.begin())<<"\n";
     
     return 0;
 }
