9d3411e8406e657032e07663c00a137b82192168
woozoo_zinc <your_zinc@ewhain.net>
2021-10-01 10:49:43 +0900

    [백트래킹] 9월 24일 - 15650.cpp 주석(라이브코딩)

---
+++ 9월 24일 - 백트래킹/15650.cpp
@@ -0,0 +1,47 @@
+// 9월 24일 - 백트래킹
+// 라이브 코딩
+
+#include <iostream>
+
+using namespace std;
+
+int n,m; //n= 1~n 까지 m= m개의 숫자
+int size[8]; //size[]= 만든 순열 저장
+bool checked[9]; ///checked[]= 순열 만들 때 사용한 숫자 check하는 것
+
+void solution(int step, int k) // k= 현재까지 사용한 숫자 중 제일 큰 수 
+{
+    if(step==m) // 마지막 순열 자리+1 일 때
+    {
+        // print
+        for(int i=0; i<step; i++)
+            cout<<size[i]<<" "; // 순열 출력
+        cout<<"\n";
+        
+    }
+    else
+    {
+        for(int i=k; i<=n; i++) //1부터 n까지의 숫자 반복
+        {
+            if(!checked[i]) //i가 사용되지 않았다면
+            {
+                size[step]=i; // 만들고 있는 순열 뒷자리에 i 를 추가
+                checked[i]=true; // i 사용 표시 
+                solution(step+1,i); // (재귀) - 순열의 다음 자리 만들기
+                checked[i]=false; // i 사용 표시 해제
+            }
+        }
+        
+    }
+}
+
+int main()
+{
+    //input
+    cin>>n>>m;
+    
+    //solution
+    solution(0,1);
+    
+    return 0;
+}
